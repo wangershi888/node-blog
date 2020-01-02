bc996dcd9f790a3c85de0ce07de3fc0130f0e69b
wwk5052 <wangwenkai5052@outlook.com>
2020-01-02 11:18:57 +0800

    asdf

--- src/db/mysql.js
+++ src/db/mysql.js
@@ -1,23 +1,25 @@
 const mysql = require('mysql');
-const {MYSQL_CONF} = require('../conf/db');
+const {
+  MYSQL_CONF
+} = require('../conf/db');
 
 const con = mysql.createConnection(MYSQL_CONF);
 
-con.connect();
+// con.connect();
 
-function exec(sql){
-    const promise = new Promise((resolve,reject)=> {
-        con.query(sql,(err,result) => {
-            if(err){
-                reject(err);
-                return;
-            }
-            resolve(result);
-        });
+function exec(sql) {
+  const promise = new Promise((resolve, reject) => {
+    con.query(sql, (err, result) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(result);
     });
-    return promise;
+  });
+  return promise;
 }
 
 module.exports = {
-    exec
+  exec
 }
